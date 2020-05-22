b24e4055277b36f7b1ecbac1a1f05cdd8afaff0b
Nilo Neto <nilo.neto@avenuecode.com>
2020-05-22 19:02:48 -0300

    added notification icon

--- src/Notification/Notification.jsx
+++ src/Notification/Notification.jsx
@@ -1,7 +1,12 @@
 // @flow
 import * as React from 'react';
+import Bell from '../assets/bell.svg';
 import './style.scss';
 
-const Notification = () => <div>notf</div>;
+const Notification = () => (
+  <div>
+    <img className="" src={Bell} alt="Avenue Code logo" />
+  </div>
+);
 
 export default Notification;
