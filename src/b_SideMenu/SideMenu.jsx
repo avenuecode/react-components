3e98f0dce473aaed6170bb0123a01cc3dd7471b8
renancamponez <rcamponez@avenuecode.com>
2020-06-17 12:43:39 -0700

    Implementing the suggestion

--- src/b_SideMenu/SideMenu.jsx
+++ src/b_SideMenu/SideMenu.jsx
@@ -41,9 +41,7 @@ class SideMenu extends React.Component<Props> {
                 role="button"
                 key={item.id}
                 to={item.path}
-                classNames(['ac-side-menu-item', {'active': this.state.currentItem === item.id}, classList])
-                  this.state.currentItem === item.id ? 'active' : ''
-                }`,classList)}
+                className={classNames('ac-side-menu-item',`${this.state.currentItem === item.id ? 'active':''}`, classList)}
                 onClick={() => {
                   this.setState({ currentItem: item.id });
                 }}
